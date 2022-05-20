2ff86ed15a9fb1a1bfd4b740b639c830f4ac8285
NoahL <lauffern@gmail.com>
2022-05-20 10:40:01 -0300

    add: generate auth tokens for register and login pages using jsonwebtoken

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,15 @@
 const User = require('../models').User
-
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config/app')
 
 exports.login = async (req, res) => {
 
   const { email, password } = req.body
 
   try {
+
+        const secret = require('crypto').randomBytes(64).toString('hex')
         //find user
         const user = await User.findOne({
           where: {
@@ -13,14 +17,24 @@ exports.login = async (req, res) => {
           }
 
         }) 
-        return res.send(user)
         // check if user in DB
 
+        if (!user) return res.status(404).json({message: 'user not found!'})
+        
         //check if password in DB and matches user
 
+        if (!bcrypt.compareSync(password, user.password)) return res.status(401).json({message: 'incorrect password!'})
+
         // gen auth token 
+        
+        const userWithToken = generateToken(user.get({raw: true}))
+
+        return res.send(userWithToken)
+        
+
   } catch(e) {
 
+    return res.status(500).json({ message: e.message })
   }
   
   return res.send([email, password])
@@ -28,5 +42,30 @@ exports.login = async (req, res) => {
 }
 
 exports.register = async (req, res) => {
+
+  try {
+    const user = await User.create(req.body)
+            
+    const userWithToken = generateToken(user.get({raw: true}))
+
+    return res.send(userWithToken)
+    
+  } catch (e) {
+    return res.status(500).json({ message: e.message })
+  }
   
+}
+
+const generateToken = (user) => {
+  
+  console.log(user)
+
+  delete user.password
+
+  
+
+  const token = jwt.sign(user, config.appKey, {expiresIn: 86400})
+
+  return {...user, ...{token}}
+
 }
