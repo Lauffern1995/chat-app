9dd80c3e6595fa639887c8c587821c8ab51f194e
NoahL <lauffern@gmail.com>
2022-06-01 10:39:12 -0300

    add: express-validator pkg

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,70 +2,68 @@ const User = require('../models').User
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const config = require('../config/app')
+const {validationResult} = require('express-validator')
 
 exports.login = async (req, res) => {
+    const { email, password } = req.body
 
-  const { email, password } = req.body
-
-  try {
-
+    try {
         const secret = require('crypto').randomBytes(64).toString('hex')
         //find user
         const user = await User.findOne({
-          where: {
-            email
-          }
-
-        }) 
+            where: {
+                email,
+            },
+        })
         // check if user in DB
 
-        if (!user) return res.status(404).json({message: 'user not found!'})
-        
+        if (!user) return res.status(404).json({ message: 'user not found!' })
+
         //check if password in DB and matches user
 
-        if (!bcrypt.compareSync(password, user.password)) return res.status(401).json({message: 'incorrect password!'})
+        if (!bcrypt.compareSync(password, user.password))
+            return res.status(401).json({ message: 'incorrect password!' })
 
-        // gen auth token 
-        
-        const userWithToken = generateToken(user.get({raw: true}))
+        // gen auth token
+
+        const userWithToken = generateToken(user.get({ raw: true }))
 
         return res.send(userWithToken)
-        
+    } catch (e) {
+        return res.status(500).json({ message: e.message })
+    }
 
-  } catch(e) {
-
-    return res.status(500).json({ message: e.message })
-  }
-  
-  return res.send([email, password])
-
+    return res.send([email, password])
 }
 
 exports.register = async (req, res) => {
 
-  try {
-    const user = await User.create(req.body)
-            
-    const userWithToken = generateToken(user.get({raw: true}))
-
-    return res.send(userWithToken)
-    
-  } catch (e) {
-    return res.status(500).json({ message: e.message })
-  }
   
+  
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        const user = await User.create(req.body)
+
+        const userWithToken = generateToken(user.get({ raw: true }))
+
+        return res.send(userWithToken)
+    } catch (e) {
+        return res.status(500).json({ message: e.message })
+    }
 }
 
 const generateToken = (user) => {
-  
-  console.log(user)
+    console.log(user)
 
-  delete user.password
+    // delete user.password
 
-  
+    const token = jwt.sign(user, config.appKey, { expiresIn: 86400 })
 
-  const token = jwt.sign(user, config.appKey, {expiresIn: 86400})
-
-  return {...user, ...{token}}
-
+    return { ...user, ...{ token } }
 }
