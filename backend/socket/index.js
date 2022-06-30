da31250710ead538aad93db18cc890e1ee09b9ea
NoahL <lauffern@gmail.com>
2022-06-30 14:29:27 -0300

    add: get chatters func to query DB for online friends

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -1,5 +1,7 @@
 const socketIo = require('socket.io')
 
+const { sequelize } = require('../models')
+
 const users = new Map()
 
 const SocketServer = (server) => {
@@ -18,11 +20,58 @@ const SocketServer = (server) => {
                 users.set(user.id, { id: user.id, sockets: [socket.id] })
                 sockets.push(socket.id)
             }
-            console.log('New User Joined', user.firstName)
+            const onlineFriends = [] //ids
 
-            io.to(socket.id).emit('typing', 'User Typing...')
+            const chatters =  await getChatters(user.id)
+            
+            //query
+
+            console.log(chatters);
+            
+
+            //notify friends that user is now online.
+            for (let i = 0; i < chatters.length; i++) {
+                if (users.has(chatters[i])) {
+                    const chatter = users.get(chatters[i])
+                    chatters.sockets.forEach((socket) => {
+                        try {
+                            io.to(socket).emit('online', user)
+                        } catch (e) {}
+                    })
+                    onlineFriends.push(chatter.id)
+                }
+            }
+            // send to user sockets which of their friends are online
+            sockets.forEach((socket) => {
+                try {
+                    io.to(socket).emit('friends', onlineFriends)
+                } catch (e) {}
+            })
         })
     })
+}
+
+const getChatters = async (userId) => {
+    try {
+        const [results, metadata] = await sequelize.query(`
+      select "cu"."userId" from "ChatUsers" as cu
+      inner join (
+        select "c"."id" from "Chats" as c
+        where exists (
+          select "u"."id" from "Users" as u
+          inner join "ChatUsers" on u.id = "ChatUsers"."userId"
+          where u.id = ${parseInt(userId)} and c.id = "ChatUsers"."chatId"
+        )
+      ) as cjoin on cjoin.id = "cu"."chatId"
+      where "cu"."userId" != ${parseInt(userId)}
+    `)
+
+        return results.length > 0 ? results.map((el) => el.userId) : []
+    } catch (e) {
+        console.log('ERR', e)
+
+        return [] 
+    }
 }
 
 module.exports = SocketServer
